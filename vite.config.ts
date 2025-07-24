b3d4816bac1dbfae33804d468976e04657ebd304
caitlynfteco <caitlynlaneeco@gmail.com>
2025-07-24 11:00:07 -0400

    pushing background history changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    viteSingleFile(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
