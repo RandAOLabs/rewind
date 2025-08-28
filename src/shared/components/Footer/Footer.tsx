a4090c247fb8c9ed577338a7a9a5908e2de5df39
caitlynfteco <caitlynlaneeco@gmail.com>
2025-08-28 06:06:14 -0400

    navigational update

--- src/shared/components/Footer/Footer.tsx
+++ src/shared/components/Footer/Footer.tsx
@@ -47,7 +47,7 @@ export default function Footer() {
             className="footer-button"
             onClick={() => setShowChangeLog(true)}
           >
-            {`${"APP_VERSION"}-${"0.4.17"}`}
+            {`${"APP_VERSION"}-${"0.4.18"}`}
           </button>
         </Tooltip>
 
